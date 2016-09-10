3c0a8e40e5a6f935fc1a898f3184c9cba4a0686a
Afaren <550047450@qq.com>
2016-09-10 21:19:54 +0800

    afaren - 2nd test case of getSnapshot

--- spec/snapshot-spec.js
+++ spec/snapshot-spec.js
@@ -25,6 +25,27 @@ cat1 12 8 3 4`;
       expect(getSnapshot(historyData, id)).toEqual(expectedSnapshots);
 
     });
+
+    it('should return snapshots of animals when given another id', () => {
+      const historyData = `e4e87cb2-8e9a-4749-abb6-26c59344dfee
+2016/09/02 22:30:46
+cat1 10 9
+
+351055db-33e6-4f9b-bfe1-16f1ac446ac1
+2016/09/02 22:30:52
+cat1 10 9 2 -1
+cat2 2 3
+
+dcfa0c7a-5855-4ed2-bc8c-4accae8bd155
+2016/09/02 22:31:02
+cat1 12 8 3 4`;
+
+      const expectedSnapshots = 'cat1 10 9';
+      const id = 'e4e87cb2-8e9a-4749-abb6-26c59344dfee';
+
+      expect(getSnapshot(historyData, id)).toEqual(expectedSnapshots);
+
+    });
   });
 
 });
